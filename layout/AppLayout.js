16cf5a3789b2ec2dcba2bd64a59483a9eeb54fa9
Ronan Ian D. Sui <39039775+iansui2@users.noreply.github.com>
2024-04-30 08:53:24 +0800

    update on footer

--- layout/AppLayout.js
+++ layout/AppLayout.js
@@ -1,13 +1,10 @@
 import { 
   Box, Container, HStack, Image, Heading, Text,
-  IconButton, useColorMode, useColorModeValue as mode, Link, Button,
-  AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader,
-  AlertDialogContent, AlertDialogOverlay, useDisclosure
+  IconButton, useColorMode, useColorModeValue as mode, Link, Button
 } from "@chakra-ui/react";
 import { IoSunnyOutline, IoMoonOutline } from 'react-icons/io5'
 
 export const AppLayout = ({ children }) => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
   const { colorMode, toggleColorMode } = useColorMode()
 
   return (
@@ -39,41 +36,9 @@ export const AppLayout = ({ children }) => {
       </Box>  
       <Box bg={mode('white', 'gray.900')} pos="fixed" bottom={0} w="full" boxShadow="xl" backdropFilter="blur(20px)" zIndex="1000" py={4}>
         <Container maxW="container.xl">
-          <HStack w="full" align="center" justify="space-between">
+          <HStack w="full" align="center" justify="center">
             <Text fontWeight="bold">Powered by Vercel</Text>
-            <Button variant="link" color={mode('black', 'white')} _hover={{ color: 'orange.500' }} _active={{ color: 'orange.500' }} onClick={onOpen}>Credits</Button>
           </HStack>
-          <AlertDialog
-            isOpen={isOpen}
-            onClose={onClose}>
-            <AlertDialogOverlay>
-              <AlertDialogContent bg={mode('white', 'gray.900')}>
-                <AlertDialogHeader>
-                  Credits
-                </AlertDialogHeader>
-                <AlertDialogBody>
-                  Credits for the free graphql endpoint: 
-                  <br /><Text as="a" target="_blank" href="https://graphql-compose.herokuapp.com/northwind" fontWeight="bold">https://graphql-compose.herokuapp.com/northwind</Text>.
-                  <br />This is the graphql endpoint used in this project.
-                </AlertDialogBody>
-                <AlertDialogFooter>
-                  <Button
-                  _hover={{ bgColor: 'orange.200', transform: 'scale(1.05)', transition: 'all 300ms ease' }}
-                  _active={{ bgColor: 'orange.200' }}
-                  _focus={{ borderColor: 'orange.500' }} 
-                  color="white"
-                  bgColor="orange.500" 
-                  size="lg"
-                  rounded="full" 
-                  onClick={() => {
-                    onClose()
-                  }}>
-                    Okay
-                  </Button>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialogOverlay>
-          </AlertDialog>
         </Container> 
       </Box>
     </Box>
